01653b2a6de279a07acea85ad4516b2d4b1a8cee
Dmitry Kondar <dmitry.kondar@speedandfunction.com>
2024-01-25 10:43:19 +0200

    add create-user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,48 @@
-export class CreateUserDto {}
+import {
+  MinLength,
+  IsString,
+  IsNotEmpty,
+  IsAlphanumeric,
+  IsEmail,
+  IsInt,
+  IsEnum,
+  Matches,
+} from 'class-validator';
+
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$/;
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2, { message: 'You must have at least two characters' })
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @MinLength(3, { message: 'Username must have atleast 3 characters.' })
+  @IsAlphanumeric(null, {
+    message: 'Username does not allow other than alpha numeric chars.',
+  })
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail(null, { message: 'Please provide a valid Email.' })
+  email: string;
+
+  @IsInt()
+  age: number;
+
+  @IsString()
+  @IsEnum(['f', 'm', 'u'])
+  gender: string;
+
+  @IsNotEmpty()
+  @Matches(passwordRegex, {
+    message: `Password must contain Minimum 8 and maximum 20 characters, 
+    at least one uppercase letter, 
+    one lowercase letter, 
+    one number and 
+    one special character`,
+  })
+  password: string;
+}
